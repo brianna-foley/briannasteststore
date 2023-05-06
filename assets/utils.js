1fb22e8c890c60fc9e3ae35e25a614934736c017
Brianna Foley <92998577+brianna-foley@users.noreply.github.com>
2023-05-05 18:50:42 -0600

    theme download and features merge

---
+++ assets/utils.js
@@ -0,0 +1,3 @@
+export const toggleClass = (element, elementClass) => element.classList.toggle(elementClass)
+export const addClass = (element, classToAdd) => element.classList.add(classToAdd)
+export const keyPressIs = (keyCode, event) => event.code === keyCode 
