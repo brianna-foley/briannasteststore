0b090f2f7a88b0596b7ba68a15c2d4740204cba5
Brianna Foley <92998577+brianna-foley@users.noreply.github.com>
2023-05-09 14:46:43 -0600

    delete previous theme

--- assets/utils.js
+++
@@ -1,3 +0,0 @@
-export const toggleClass = (element, elementClass) => element.classList.toggle(elementClass)
-export const addClass = (element, classToAdd) => element.classList.add(classToAdd)
-export const keyPressIs = (keyCode, event) => event.code === keyCode 
